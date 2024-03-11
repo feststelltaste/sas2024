cc1fbe978ea1e293289693ad2a021bee1007d6a1
Markus Harrer <github@markusharrer.de>
2024-03-11 23:40:47 +0100

    change config

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -2,13 +2,13 @@
 
 c.ServerProxy.servers = {
     'neo4j': {
-        'command' : None,
-        'absolute_url' : True,
-        'port': 7474,
-        'timeout': 60,
-        'launcher_entry': {
+        'command': None,  # Da Neo4j bereits läuft, kein Startbefehl nötig
+        'port': 7474,  # Port, auf dem Neo4j lauscht
+        'absolute_url': False,  # Relative URLs innerhalb von Jupyter verwenden
+        'timeout': 60,  # Timeout für Verbindungsaufbau, kann angepasst werden
+        'launcher_entry': {  # Eintrag im Jupyter Launcher
             'enabled': True,
-            'title': 'Neo4j Session'
+            'title': 'Neo4j Browser'
         }
     }
 }
